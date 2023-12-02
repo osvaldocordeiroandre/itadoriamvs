552a2ee2341f17d82d436041714f8a28684ddd83
Osvaldo Cordeiro André <94922391+osvaldocordeiroandre@users.noreply.github.com>
2023-12-02 20:26:50 -0300

    adicionando novo corte

--- src/Pages/Clips/clips.js
+++ src/Pages/Clips/clips.js
@@ -11,6 +11,7 @@ import ScrollToTop from 'react-scroll-to-top';
 
 export default function Clips() {
   const animes = [
+    { id: '76', nome: 'Ragna Crimson, ', imagem: 'https://cdn.discordapp.com/attachments/1135116775051239434/1180650436668170280/Ragna-Crimson-S1.webp?ex=657e3167&is=656bbc67&hm=873b995cf81269eede224c695e24f88fa5bb05cbbfd7a2931b82946b6e96f4bf&', link: 'https://www.playbook.com/s/ajksz/eNFdihFCFStXUnYVaeux45tZ' },
     { id: '75', nome: 'Violet Movies', imagem: 'https://cdn.discordapp.com/attachments/1135116775051239434/1169108988886003732/Violet-Movies.webp?ex=65543497&is=6541bf97&hm=d5a007cc2bade5d8c3cb4de4f0bc60527c10db61ef013968d3b175d5740dfc4c&', link: 'https://www.playbook.com/s/ajksz/QaSN9y1br7G8ymCf9VWj4GgB' },
     { id: '74', nome: 'Violet Evergarden', imagem: 'https://cdn.discordapp.com/attachments/1135116775051239434/1169108969474769016/Violet-S1.webp?ex=65543493&is=6541bf93&hm=cc9ce657556823bbf61d655759cb906de16d2366a4a8630d78a4a99df37e8367&', link: 'https://www.playbook.com/s/ajksz/kBdr4JkLjwB4kQS3XB2kihyz' },
     { id: '73', nome: 'Jujutsu Kaisen 0', imagem: 'https://cdn.discordapp.com/attachments/1135116775051239434/1139899403021320242/jujutsu-kaisen-0.webp', link: 'https://www.playbook.com/s/ajksz/KGCYWcyNQDCWfNmhjCFJHq79' },
