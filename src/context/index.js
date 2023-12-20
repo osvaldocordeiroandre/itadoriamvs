6a864b52e66a46e556e536508e28ab7789a02ea8
Osvaldo Cordeiro André <94922391+osvaldocordeiroandre@users.noreply.github.com>
2023-12-20 15:23:02 -0300

    adicionando dark mode no local storage e arrumando bug de animações

--- src/context/index.js
+++ src/context/index.js
@@ -1,9 +1,16 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
 
 export const DarkModeContext = createContext();
 
 export const DarkModeProvider = ({ children }) => {
-  const [darkMode, setDarkMode] = useState(false);
+  const storeDarkMode = localStorage.getItem('darkMode') === 'true';
+  const [darkMode, setDarkMode] = useState(storeDarkMode);
+
+  useEffect(() => {
+
+    localStorage.setItem('darkMode', darkMode.toString());
+
+  }, [darkMode]);
 
   return (
     <DarkModeContext.Provider value={[darkMode, setDarkMode]}>
