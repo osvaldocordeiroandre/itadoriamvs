0788c967933a50c41f6a40e9a9e0c1eb2ff2cc9e
Osvaldo Cordeiro André <94922391+osvaldocordeiroandre@users.noreply.github.com>
2023-12-30 00:26:23 -0300

    adicionando mais videos aleatorio

---
+++ src/Assets/Data/videoUrls.jsx
@@ -0,0 +1,29 @@
+const videoUrls = [
+
+    'https://youtu.be/bTEJH35-cDE',
+    'https://youtu.be/PM0oJ2AtUxA',
+    'https://youtu.be/y6nwXQ7Au8M',
+    'https://youtu.be/As9hsoJF7gA',
+    'https://youtu.be/YyhnxX9Tuns',
+    'https://youtu.be/y16StPq-Rm4',
+    'https://youtu.be/Q8bpBJM77dE',
+    'https://youtu.be/rZj7QKlqGig',
+    'https://youtu.be/60ewJ2yEoNI',
+    'https://www.youtube.com/watch?v=SqcUixL97d0',
+    'https://www.youtube.com/watch?v=rZj7QKlqGig',
+    'https://www.youtube.com/watch?v=60ewJ2yEoNI',
+    'https://www.youtube.com/watch?v=pVDUSwCOAO0',
+    'https://www.youtube.com/watch?v=-H9G7EzwShE',
+    'https://www.youtube.com/watch?v=JokUMSmOxfg',
+    'https://www.youtube.com/watch?v=Zu4KpfFoCow',
+    'https://www.youtube.com/watch?v=E-UVvnaSVvw',
+    'https://www.youtube.com/watch?v=KqtYrvbTKzU',
+    'https://www.youtube.com/watch?v=067S_uyHD_Y',
+    'https://www.youtube.com/watch?v=wNxRo7S73PQ',
+    'https://www.youtube.com/watch?v=AvNkiJsM30s',
+    'https://www.youtube.com/watch?v=c6YHXtHzvQg',
+    'https://www.youtube.com/watch?v=12vU0ZxmVFY',
+
+  ];
+
+  export default videoUrls;
--- src/components/RandomVideo/Randonvideo.js
+++ src/components/RandomVideo/Randonvideo.js
@@ -4,30 +4,9 @@ import '../../components scss/randomvideo.scss'
 import { GiPerspectiveDiceSixFacesRandom } from 'react-icons/gi'
 import { DarkModeContext } from '../../context';
 
+import videoUrls from '../../Assets/Data/videoUrls';
+
 export default function Randonvideo() {
-
-  const videoUrls = [
-
-    'https://youtu.be/y16StPq-Rm4',
-    'https://youtu.be/Q8bpBJM77dE',
-    'https://youtu.be/rZj7QKlqGig',
-    'https://youtu.be/60ewJ2yEoNI',
-    'https://www.youtube.com/watch?v=SqcUixL97d0',
-    'https://www.youtube.com/watch?v=rZj7QKlqGig',
-    'https://www.youtube.com/watch?v=60ewJ2yEoNI',
-    'https://www.youtube.com/watch?v=pVDUSwCOAO0',
-    'https://www.youtube.com/watch?v=-H9G7EzwShE',
-    'https://www.youtube.com/watch?v=JokUMSmOxfg',
-    'https://www.youtube.com/watch?v=Zu4KpfFoCow',
-    'https://www.youtube.com/watch?v=E-UVvnaSVvw',
-    'https://www.youtube.com/watch?v=KqtYrvbTKzU',
-    'https://www.youtube.com/watch?v=067S_uyHD_Y',
-    'https://www.youtube.com/watch?v=wNxRo7S73PQ',
-    'https://www.youtube.com/watch?v=AvNkiJsM30s',
-    'https://www.youtube.com/watch?v=c6YHXtHzvQg',
-    'https://www.youtube.com/watch?v=12vU0ZxmVFY',
-
-  ];
 
   const handleButtonClick = () => {
     const randomVideoindex = Math.floor(Math.random() * videoUrls.length);
